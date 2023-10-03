40ffd544735952def1f0b5041391a1ca38a4b1f9
DupLeo <leodupraz.r@gmail.com>
2023-10-03 15:51:40 +0200

    buttom

--- app/src/main/java/com/example/plantea/MainActivity.kt
+++ app/src/main/java/com/example/plantea/MainActivity.kt
@@ -15,32 +15,6 @@ import com.example.plantea.ui.theme.PlanteaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PlanteaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "HEYO $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlanteaTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
