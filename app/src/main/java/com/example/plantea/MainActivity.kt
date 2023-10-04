657df94027b1b2b99081b24c3c47dcb55d0fcef6
DupLeo <leodupraz.r@gmail.com>
2023-10-04 17:47:24 +0200

    nav qui marche

--- app/src/main/java/com/example/plantea/MainActivity.kt
+++ app/src/main/java/com/example/plantea/MainActivity.kt
@@ -4,15 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.plantea.ui.theme.PlanteaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +18,4 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
     }
-
 }
