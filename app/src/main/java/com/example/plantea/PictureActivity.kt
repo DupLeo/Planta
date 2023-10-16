1a1ceaf044f97c8bcfbcac19448a0b2fbf9a030d
DupLeo <leodupraz.r@gmail.com>
2023-10-16 23:33:49 +0200

    correction sur l'api 27

--- app/src/main/java/com/example/plantea/PictureActivity.kt
+++ app/src/main/java/com/example/plantea/PictureActivity.kt
@@ -9,31 +9,26 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import com.example.plantea.model.Plante
 import com.example.plantea.storage.PlanteDataBaseStorage
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import java.util.Locale.*
+import java.util.Random
 
 
 class PictureActivity : AppCompatActivity() {
     private lateinit var imagePhoto: ImageView
     private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_WRITE_EXTERNAL_STORAGE = 2
+    private val REQUEST_MANAGE_EXTERNAL_STORAGE = 3
     private var imageCaptured = false // Variable de drapeau
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,6 +65,7 @@ class PictureActivity : AppCompatActivity() {
                 }
             }
 
+
         // demande l'autorisation pour prendre une photo
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
             != PackageManager.PERMISSION_GRANTED) {
@@ -80,82 +76,80 @@ class PictureActivity : AppCompatActivity() {
                 REQUEST_CAMERA_PERMISSION
             )
         }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Demande la permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQUEST_WRITE_EXTERNAL_STORAGE
+            )
+        }
         imagePhoto.setOnClickListener { takePhoto.launch(null) }
+
 
         //===========================   SAUVEGARDER UNE PHOTO     ================================//
 
         buttonSave.setOnClickListener {
-            // Sauvegarde l'image ici, si nécessaire
             if (imageCaptured) {
-                // Une image a été capturée, vous pouvez la sauvegarder
-                saveImageToFile(imagePhoto.drawable.toBitmap())
-
-
-
+                val drawable = imagePhoto.drawable
+                if (drawable != null) {
+                    saveImageToFile(drawable.toBitmap())
+                } else {
+                    showToast("L'image capturée est nulle.")
+                }
             } else {
-                // Aucune image n'a été capturée, affichez un message d'erreur
                 showToast("Prenez une photo")
             }
         }
 
 
-    }
 
+    }
 
     // Enregistre une photo dans la galerie
-    private fun saveImageToFile(bitmap: Bitmap) {
-        val nomCommun = findViewById<EditText>(R.id.InputNom).text.toString()
-        val nomDeFamille = findViewById<EditText>(R.id.InputNomDeFamille).text.toString()
-
-        if (nomCommun.isEmpty() && nomDeFamille.isEmpty()) {
-            showToast("Veuillez saisir un nom commun et un nom de famille avant de sauvegarder l'image.")
-            return
-        }
-        val imageFileName = "${nomCommun}_${nomDeFamille}.jpg"
-
-        val contentValues = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, imageFileName)
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-        }
-
-        val resolver = contentResolver
-        val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
+    private fun saveImageToFile(finalBitmap: Bitmap) {
+        val root = Environment.getExternalStorageDirectory().toString()
+        val myDir = File("$root/saved_images")
+        myDir.mkdirs()
+        val generator = Random()
+        var n = 10000
+        n = generator.nextInt(n)
+        val fname = "Image-$n.jpg"
+        val file = File(myDir, fname)
+        if (file.exists()) file.delete()
         try {
-            val outputStream = resolver.openOutputStream(imageUri!!)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            outputStream?.close()
-
-            // Récupérer l'URI de l'image sauvegardée
-            val savedImageUri = imageUri.toString()
-            //showToast("Image enregistrée avec succès. Lien : $savedImageUri")
-
-            // Créez un objet Plante avec les informations nécessaires
-            val plante = Plante(
-                id = 0,
-                name = nomCommun,
-                famille = nomDeFamille,
-                photo = savedImageUri
-            )
-
-            // Insérez la plante dans la base de données
-            val planteStorage = PlanteDataBaseStorage(this)
-            val insertedId = planteStorage.insert(plante)
-
-            if (insertedId != -1) {
-                showToast("Plante enregistrée avec succès. ID: $insertedId")
-            } else {
-                showToast("Échec de l'enregistrement de la plante.")
-            }
-
-            // Réinitialiser le drapeau imageCaptured à false
-            imageCaptured = false
-        } catch (e: IOException) {
+            val out = FileOutputStream(file)
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+            out.flush()
+            out.close()
+            val filePath = file.absolutePath
+            handlePlanteInsertion("nom_1", "famille_1", filePath)
+        } catch (e: Exception) {
             e.printStackTrace()
-            showToast("Échec de l'enregistrement de l'image.")
         }
     }
+
+    private fun handlePlanteInsertion(nomCommun: String, nomDeFamille: String, savedImageUri: String) {
+        val plante = Plante(
+            id = 0,
+            name = nomCommun,
+            famille = nomDeFamille,
+            photo = savedImageUri
+        )
+
+        val planteStorage = PlanteDataBaseStorage(this)
+        val insertedId = planteStorage.insert(plante)
+
+        if (insertedId != -1) {
+            showToast("Plante enregistrée avec succès. ID: $insertedId")
+        } else {
+            showToast("Échec de l'enregistrement de la plante.")
+        }
+
+        imageCaptured = false
+    }
+
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
